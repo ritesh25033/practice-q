c75b044a4e49998a30aa970aec2204871ef0ab2c
ritesh <ritesh25033@gmail.com>
2024-04-15 11:10:52 +0530

    navbar

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -4,7 +4,9 @@ import LogoImg from '../../assets/logo.png';
 
 const Logo = () => {
   return (
+    <div className='logoDiv'>
       <img src={LogoImg} width={67} alt='logo' />
+    </div>
   );
 };
 
